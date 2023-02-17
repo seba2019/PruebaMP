e0f0a337465c89859fff9d6a7bd2d834ade34eb4
Sebastian Vega - Unica <mrsavl1@gmail.com>
2023-02-17 16:47:37 -0300

    implementacion de preferencias

--- PruebaMP/Controllers/HomeController.cs
+++ PruebaMP/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MercadoPago.Client.Preference;
+using MercadoPago.Config;
+using MercadoPago.Resource.Preference;
+using Microsoft.AspNetCore.Mvc;
 using PruebaMP.Models;
 using System.Diagnostics;
 
@@ -18,15 +21,33 @@ namespace PruebaMP.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<JsonResult> CreateOrder() 
+        {
+            MercadoPagoConfig.AccessToken = "TEST-2781679229953676-021710-929b51994d8ef51c573b53e6293b4d5c-1045528840";
+            var request = new PreferenceRequest
+            {
+                Items = new List<PreferenceItemRequest>
+                    {
+                        new PreferenceItemRequest
+                        {
+                            Title = "Mi producto",
+                            Quantity = 1,
+                            CurrencyId = "ARS",
+                            UnitPrice = 75.56m,
+                        },
+                    },
+            };
+
+            // Crea la preferencia usando el client
+            var client = new PreferenceClient();
+            Preference preference = await client.CreateAsync(request);
+            return Json(preference);
         }
     }
 }
--- PruebaMP/wwwroot/js/site.js
+++ PruebaMP/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(() => {
+    $.ajax({
+        url: "Home/CreateOrder",
+        method: "get",
+        success: (e) => {
+            console.log(e);
+        },
+        error: (e) => {
+            console.log(e);
+        }
+    })
+});
